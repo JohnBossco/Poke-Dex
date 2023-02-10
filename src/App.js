e757c9db2f09b341cd642fd6f78f714fab8123cb
johnbosco muiruri <johnmuiruri352@gmail.com>
2023-02-10 04:05:06 -0600

    fixed search bar start of bottom half page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Navbar from './nav';
 import Pokedexheader from './poke-dex-header';
-import Searchbar from './searchBar';
 
 
 
@@ -11,7 +10,6 @@ function App() {
     <div className="App">
       <Pokedexheader />
       <Navbar/>
-      <Searchbar/>
     </div>
   );
 }
--- src/nav.jsx
+++ src/nav.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./nav.css";
+import Searchbar from './searchBar';
+
 
 export default function Navbar() {
   return (
@@ -8,10 +10,11 @@ export default function Navbar() {
         <div className="info">
           <h1>Info</h1>
           <div className="infoText">
-             A pokedex is basically a dictionary for the Pokemon it has all the
+            A pokèdex is basically a dictionary for the Pokemon it has all the
             info for all 1008 poke from type, abilities, moves and much more.
           </div>
         </div>
+        <Searchbar className="mainSearchBar" />
         <div className="howToUse">
           <h1>How To Use</h1>
           <div className="infoText">
